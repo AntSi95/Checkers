61e58f32a85871f9ff799b05f656ca6efcbc7c10
AntSi95 <antonsi95@yandex.ru>
2026-05-30 11:27:33 +0300

    docs: add real TODO comments for deadcode and optimization

--- Checkers/Scanning/MoveScanContext.cs
+++ Checkers/Scanning/MoveScanContext.cs
@@ -207,10 +207,14 @@ namespace Checkers.Engine.Scanning
             State = IsCaptureMode ? ScanState.ForcedCaptureOnly : ScanState.Default;
         }
 
+        // TODO [Optimization]: Активировать и интегрировать этот метод при переходе к глубокому сканированию, при необходимости
         ///// <summary>
         ///// Полностью очищает контекст и готовит его к новой независимой сессии поиска ходов.
         ///// </summary>
-        ///// <remarks> Не забываем вызываеть его после конструктора</remarks>
+        ///// <remarks>
+        ///// Позволяет повторно использовать существующий экземпляр контекста для другой фигуры,
+        ///// минимизируя нагрузку на сборщик мусора (GC) в глубоких циклах поиска.
+        ///// </remarks>
         //public void RestartScanning(Point startSquare, ScanMode mode)
         //{
         //    FoundMoves.Clear();
